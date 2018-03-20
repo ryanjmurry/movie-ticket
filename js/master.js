36d952e80d275943db55e70be601d8fc7f81b0e5
Ryan Murry <ryanjmurry@gmail.com>
2018-03-20 11:20:14 -0700

    add html and js files so that a user can select the movie, time, and discount. Web page will respond by generating a ticket and price.

---
+++ js/master.js
@@ -0,0 +1,43 @@
+//Business Logic
+function Ticket(movie, time, discount) {
+  this.movie = movie;
+  this.time = time;
+  this.discount = discount;
+};
+
+Ticket.prototype.Price = function() {
+  var price = 10;
+  if (this.movie === "Ready Player One" || this.movie === "A Beautiful Day") {
+    if (this.time === "10:00 AM" || this.time === "1:00 PM") {
+      price = ((price + 5)-this.discount)*0.5
+    } else {
+      price = ((price + 5)-this.discount)
+    }
+  } else {
+    if (this.time === "10:00 AM" || this.time === "1:00 PM") {
+      price = (price-this.discount)*0.5
+    } else {
+      price = (price-this.discount)
+    }
+  }
+  return price.toFixed(2);
+};
+
+
+
+//UI Logic
+$(document).ready(function() {
+  $("form#movie-ticket").submit(function() {
+    event.preventDefault();
+    var movie = $("#movies").val();
+    var time = $("#times").val();
+    var discount = parseInt($("#discounts").val());
+    var newTicket = new Ticket(movie, time, discount);
+    var price = newTicket.Price();
+
+  $("#ticket-information").show();
+  $("#movie-name").text(newTicket.movie);
+  $("#movie-time").text(newTicket.time)
+  $("#movie-price").text(price);
+  });
+});
